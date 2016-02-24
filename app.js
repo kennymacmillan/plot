7d4dae3107f7f22272584e10c2b73f71b4c23615
Kenneth Macmillan <kennymacmillan@Kenneths-MBP.home>
2016-02-24 21:11:44 +0000

    Add express and app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello there');
+});
+
+var port = 3000;
+app.listen(port, function() {
+    console.log('App running on port ' + port);
+});
