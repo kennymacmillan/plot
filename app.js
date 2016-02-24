fb0dd1ccef4040b710beffa2d08054017d957ef2
Kenny Macmillan <kgmacmillan@gmail.com>
2016-02-24 21:49:38 +0000

    Set up .editorconfig

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello there');
+  res.send('Hello there');
 });
 
 var port = 3000;
 app.listen(port, function() {
-    console.log('App running on port ' + port);
+  console.log('App running on port ' + port);
 });
